f919826b7b105d6d46283ef9b5bfdfb3a0636f37
Álvaro Romário Cavalcante Alencar <alvaroromario1@hotmail.com>
2022-08-29 22:12:25 -0300

    feat: pushing modified files

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const router = jsonServer.router("db.json");
 app.db = router.db;
 
 const rules = auth.rewriter({
-  users: 644,
-  products: 644
+  users: 600,
+  budgets: 660
 });
 
 app.use(cors());
